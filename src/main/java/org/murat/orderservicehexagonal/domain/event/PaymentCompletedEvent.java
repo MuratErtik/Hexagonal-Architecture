396543e27a8f0c27592ef68ba3c9fd9f202dd9a3
Murat Ertik <dmuratertik1@gmail.com>
2026-07-23 00:12:07 +0300

    events added.

---
+++ src/main/java/org/murat/orderservicehexagonal/domain/event/PaymentCompletedEvent.java
@@ -0,0 +1,20 @@
+package org.murat.orderservicehexagonal.domain.event;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentCompletedEvent {
+    private UUID orderId;
+    private UUID paymentId;
+    private String SellerEmail;
+    private BigDecimal amount;
+}

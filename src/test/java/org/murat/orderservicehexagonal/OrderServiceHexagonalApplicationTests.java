bff09da83be3ac9ea8d43bb94730bc0542e40a92
Murat Ertik <dmuratertik1@gmail.com>
2026-07-22 23:29:58 +0300

    first commit

---
+++ src/test/java/org/murat/orderservicehexagonal/OrderServiceHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.murat.orderservicehexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceHexagonalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
